d9e29173726e22b293d6de32eb5c9d3edd0e2282
John Li (Tet) <tet@tet.moe>
2022-03-21 23:49:35 -0700

    added https:// support

--- src/Utils/RESTServer.ts
+++ src/Utils/RESTServer.ts
@@ -1,12 +1,13 @@
-import cors from 'cors';
-import express from 'express';
-import { readdirSync } from 'fs';
-import { lstat, readdir } from 'fs/promises';
-import { env } from '../../env';
-import { DisadusUser, RESTHandler } from '../../types/DisadusTypes';
-import SocketServer from './SocketServer';
-import { API_DOMAIN } from './constants';
-import nFetch from './fetch';
+import cors from "cors";
+import express from "express";
+import https from "https";
+import { readdirSync } from "fs";
+import { lstat, readdir } from "fs/promises";
+import { env } from "../../env";
+import { DisadusUser, RESTHandler } from "../../types/DisadusTypes";
+import SocketServer from "./SocketServer";
+import { API_DOMAIN } from "./constants";
+import nFetch from "./fetch";
 const importAllHandlers = async (path: string, server: express.Application) => {
   await Promise.all(
     (
@@ -16,7 +17,7 @@ const importAllHandlers = async (path: string, server: express.Application) => {
       if ((await lstat(`${path}/${file}`)).isDirectory()) {
         return importAllHandlers(`${path}/${file}`, server);
       }
-      if (!(file.endsWith('.ts') || file.endsWith('.js'))) {
+      if (!(file.endsWith(".ts") || file.endsWith(".js"))) {
         return;
       }
       import(`${path}/${file}`)
@@ -30,7 +31,7 @@ const importAllHandlers = async (path: string, server: express.Application) => {
           server[handler.method](handler.path, async (req, res, next) => {
             if (handler.sendUser) {
               if (!req.headers.authorization)
-                return res.status(401).send('Unauthorized');
+                return res.status(401).send("Unauthorized");
               user = await nFetch(`${API_DOMAIN}/user/@me`, {
                 headers: {
                   Authorization: req.headers.authorization,
@@ -52,8 +53,26 @@ export const RESTServer = (): express.Application => {
   server.use(express.json());
   server.use(express.urlencoded({ extended: true }));
   server.use(cors());
-  console.log('Importing REST Handlers');
+  console.log("Importing REST Handlers");
   importAllHandlers(`${__dirname}/../RESTEndpoints`, server);
-  const socketServer = new SocketServer(server.listen(env.port || 443));
+  if (env?.webserver) {
+    const httpsServer = https.createServer(
+      {
+        //@ts-ignore
+        key: readFileSync(env.webserver?.keyPath),
+        //@ts-ignore
+        cert: readFileSync(env.webserver?.certPath),
+      },
+      server
+    );
+    new SocketServer(
+      httpsServer.listen(env.port, () => {
+        console.log(`Secure HTTP Server started on port ${env.port}`);
+      })
+    );
+  } else {
+    console.log(`HTTP Server running on port ${env.port}`);
+    const SocketAPI = new SocketServer(server.listen(env.port));
+  }
   return server;
 };
